00500741d4c07a9c1c0415246b4219e15aeb828d
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-01 00:38:17 -0300

    ex 20
    Corrigido

--- python_ex2/desafio20.py
+++ python_ex2/desafio20.py
@@ -1,34 +1,14 @@
+maior = 0
+menor = 0
 
 for c in range(1, 5+1):
-    peso = float(input('Digite seu peso corporal: '))
+    peso = float(input(f'Digite o peso da {c}^ pessoa: '))
     if c == 1:
-        p1 = peso + 0
-    elif c == 2:
-        p2 = peso + 0
-    elif c == 3:
-        p3 = peso + 0
-    elif c == 4:
-        p4 = peso + 0 
-    elif c == 5:
-        p5 = peso + 0
-        if p1 > p2 and p1 > p3 and p1 > p4 and p1 > p5 :
-            print('\033[34mMaior peso lido:{}\033[m'.format(p1))     
-        if p2 > p1 and p2 > p3 and p2 > p4 and p2 > p5 :
-            print('\033[34mMaior peso lido:{}\033[m'.format(p2)) 
-        if p3 > p1 and p3 > p2 and p3 > p4 and p3 > p5 :
-            print('\033[34mMaior peso lido:{}\033[m'.format(p3)) 
-        if p4 > p2 and p4 > p3 and p4 > p1 and p4 > p5 :
-            print('\033[34mMaior peso lido:{}\033[m'.format(p4)) 
-        if p5 > p2 and p5 > p3 and p5 > p4 and p5 > p1 :
-            print('\033[34mMaior peso lido:{}\033[m'.format(p5)) 
-        
-        if p1 < p2 and p1 < p3 and p1 < p4 and p1 < p5 :
-            print('\033[35mMenor peso lido:{}\033[m'.format(p1))     
-        if p2 < p1 and p2 < p3 and p2 < p4 and p2 < p5 :
-            print('\033[35mMenor peso lido:{}\033[m'.format(p2)) 
-        if p3 < p1 and p3 < p2 and p3 < p4 and p3 < p5 :
-            print('\033[35mMenor peso lido:{}\033[m'.format(p3)) 
-        if p4 < p2 and p4 < p3 and p4 < p1 and p4 < p5 :
-            print('\033[35mMenor peso lido:{}\033[m'.format(p4)) 
-        if p5 < p2 and p5 < p3 and p5 < p4 and p5 < p1 :
-            print('\033[35mMenor peso lido:{}\033[m'.format(p5))
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print(f'O maior peso lido {maior}KG.\nO menor peso lido foi {menor}KG.')
