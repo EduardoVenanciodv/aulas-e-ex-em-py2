86fb76b7af35f8b5674ef85cf71effbe53d1c0c4
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-01 00:38:02 -0300

    ex 19
    Corrigido

--- python_ex2/desafio19.py
+++ python_ex2/desafio19.py
@@ -3,16 +3,16 @@ from datetime import date
 total1 = 0
 total2 = 0
 
-for c in range(0, 7):
-    nascimento = int(input('Digite a sua data de náscimento:'))
+for c in range(1, 8):
+    nascimento = int(input(f'Digite a data da {c}^ de náscimento:'))
     ano = date.today().year
     idade = ano - nascimento
 
     if idade >= 21:
-        print('\033[31mVocê é maior de idade.\033[m')
+        #print('\033[31mVocê é maior de idade.\033[m')
         total1 = total1 + 1
     elif idade >= 0:
-        print('\033[32mVocê é menor de idade.\033[m')
+        #print('\033[32mVocê é menor de idade.\033[m')
         total2 = total2 + 1
     else:
         print('Erro (-)')
