40b0b146927413cee7a5f6fb97537d5664be7d30
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-10 22:37:03 -0300

    ex 24
    Menu feito com while com opções de soma, mutiplicação, dizer qual é o maior numero, trocar para novos números e com saída do menu.

---
+++ python_ex2/desafio24.py
@@ -0,0 +1,29 @@
+opc = 4
+
+print('\033[33mInicio\033[m')
+while opc != 5:
+    if opc == 4:
+        v1 = int(input('Digite o primeiro valor:'))
+        v2 = int(input('Digite o segundo valor:'))
+
+    opc = int(input('\033[33mEscola umas das opções abaixo:\033[m\n'
+                    '[1]SOMA\n[2]MULTIPLICAR\n[3]MAIOR\n'
+                    '[4]NOVOS NÚMEROS\n[5]SAIR DO PROGRAMA\n'))
+
+    if opc == 1:
+        r = v1 + v2
+        print(f'\033[34mA soma de ({v1} + {v2}) =\033[m {r}')
+    elif opc == 2:
+        r = v1 * v2
+        print(f'\033[34mA multiplicação de ({v1} X {v2}) =\033[m {r}')
+
+    elif opc == 3:
+        if v1 > v2:
+            print(f'\033[34mO Maior número é o primeiro digitado:\033[m {v1}')
+        elif v1 < v2:
+            print(f'\033[34mO Maior número é o segundo digitado:\033[m {v2}')
+        else:
+            print(f'\033[34mOs números são iguais:\033[m {v1} | {v2}')
+    elif opc >= 6 or opc <= 0:
+        print('\033[31mErro!!\nEssa opção não existe!!\033[m')
+print('\033[33mFim\033[m')
