10ec0ca17634ed4e6e16ed3f387a857aa45b8df0
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-15 22:08:05 -0300

    ex 22
    Corrigido

--- python_ex2/desafio22.py
+++ python_ex2/desafio22.py
@@ -1,10 +1,5 @@
-s = ''
-while s != 'M' and s != 'F':
-    s = str(input('Digite seu sexo (M/F):')).upper()
-if s == 'F':
-    print('Seu sexo é Feminino.')
-else:
-    print('Seu sexo é Masculino.')
+s = str(input('Digite seu sexo (M/F):')).strip().upper()[0]
 
-
-
+while s not in 'MmFf':
+    s = str(input('Dados inválidos. Digite novamente seu sexo (M/F):')).strip().upper()[0]
+print(f'Seu sexo é {s}.')
