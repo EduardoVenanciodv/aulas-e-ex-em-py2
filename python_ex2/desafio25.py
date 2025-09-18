9cfb86eed6aba360e8231a4980b473559ea482f3
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-18 00:40:43 -0300

    ex 25
    Corrigido.

--- python_ex2/desafio25.py
+++ python_ex2/desafio25.py
@@ -3,12 +3,13 @@ print('\033[35mFatorial númerica.')
 n = int(input('Digite um número :\033[m '))
 print(f'\033[34m{n}! =\033[m ', end='')
 
-n2 = 1
-n += 1
+c = n
+f = 1
 
-while n != 1:
-    n -= 1
-    n2 *= n
-    print(f'\033[36m{n}', end='x')
-print(f' =\033[m {n2} ')
+while c > 0:
+    print(f'\033[36m{c}', end=' ')
+    print(' x ' if c > 1 else '=', end=' ')
+    f *= c
+    c -= 1
+print(f'\033[m{f} ')
 
