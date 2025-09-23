a030c8f64737302f7234ed2af3b63cdb2f93bd24
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-22 23:08:05 -0300

    ex 28
    Sequência de Fibonacci

--- python_ex2/desafio28.py
+++ python_ex2/desafio28.py
@@ -1,11 +1,13 @@
-n = int(input('\033[33mQuantos números você quer na sequência:\033[m '))
-cont = 0
-inic = 0
-v = 1
-while n != cont:
-    cont += 1
-    print(f'\033[34m{inic}\033[m', end='\033[31m->\033[m')
+print('Sequência de Fibonacci')
 
-    soma = v + inic
-    inic = v
-    v = soma
+n = int(input('\033[33mQuantos números você quer na sequência:\033[m '))
+cont = 1
+t1 = 0
+t2 = 1
+while cont <= n:
+    t3 = t1 + t2
+    print(f'\033[34m{t1}\033[m', end='\033[31m->\033[m')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print('FIM')
