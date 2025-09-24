c1150cdb36c609c53d97dfafc820089d82f3e9fd
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-24 00:49:51 -0300

    ex.33 Jogo do Par ou Ímpar
    Utilizando while como repetidor, e randint para o computador escolher um número aleatório de (1 ao 10).
    O usuário conseguira escolher o seu próprio número, e se ele quer ser PAR ou ÍMPAR. O programa vai fazer a somar dos valores e vai dizer se é PAR ou ÍMPAR. Se o usuário ganhar, o programa vai repetir e vai adicionar +1  vitória para o usuário. E quando o usuário perde a repetição de while vai parar com break, e no fim o programa irar dizer as vezes que o usuário conseguiu ganhar.

---
+++ python_ex2/desafio33.py
@@ -0,0 +1,37 @@
+from random import randint
+
+computador = randint(1, 10)
+cont = 0
+
+print('\033[32m/\033[33m|\033[31m\\\033[m' * 10)
+print('\033[33mVAMOS JOGAR ÍMPAR OU PAR\033[m')
+print('\033[32m/\033[33m|\033[31m\\\033[m' * 10)
+while True:
+    eu = int(input('\033[33mDigite um valor:'))
+    pi = str(input('Par ou Ímpar (P/I):\033[m')).upper().strip()[0]
+    r = computador + eu
+    if r % 2 == 0:
+        print(f'\033[33mVocê jogou {eu} e o computador {computador}. Total de {r} deu PAR\033[m ')
+        if pi == 'P':
+            print('\033[32mVocê ganhou!!')
+            print('Vamos novamente...\033[m')
+            print('--' * 20)
+            cont += 1
+        else:
+            print('\033[31mVocê Perdeu!\033[m')
+            print('--' * 20)
+            break
+    else:
+        print(f'\033[33mVocê jogou \033[m{eu}\033[33m e o computador \033[m{computador}\033[33m. '
+              f'Total de \033[m{r}\033[33m deu Ímpar\033[m')
+        if pi == 'I':
+            print('\033[32mVocê ganhou!!')
+            print('Vamos novamente...\033[m')
+            print('--' * 20)
+            cont += 1
+        else:
+            print('\033[31mVocê Perdeu!\033[m')
+            print('--' * 20)
+            break
+
+print(f'\033[31mGamer OVER!\033[33m \033[32mVocê ganhou \033[m{cont}\033[32m vez(es).')
