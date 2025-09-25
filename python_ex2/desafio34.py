9abce96977a0cbe9daba42b85f4f8dad88e4df0f
Eduardo Venancio <eduavenancio@gmail.com>
2025-09-25 19:36:39 -0300

    ex.34
    Algumas mudanças nas localizações nas estruturas.
    E a forma de escrita nos whiles .

--- python_ex2/desafio34.py
+++ python_ex2/desafio34.py
@@ -1,26 +1,25 @@
 mais18 = homens = mulher20 = 0
-print('--' *15)
-print('     \033[34mCADASTRE UMA PESSOA\033[m')
-print('--' *15)
 
 while True:
-    sexo = ''
-    continuar = ''
-
+    print('--' * 15)
+    print('     \033[34mCADASTRE UMA PESSOA\033[m')
+    print('--' * 15)
     idade = int(input('\033[34mDigite sua idade:\033[m'))
-    if idade > 18:
-        mais18 += 1
-    while sexo != 'M' and sexo != 'F':
+    sexo = ' '
+    while sexo not in 'MF':
         sexo = str(input('\033[34mDigite seu sexo (M/F):\033[m')).upper().strip()[0]
-        if sexo == 'M':
-            homens += 1
-        if sexo == 'F' and idade < 20:
-            mulher20 += 1
+    if idade >= 18:
+        mais18 += 1
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        mulher20 += 1
     print('--' * 15)
-    while continuar != 'S' and continuar != 'N' :
+    continuar = ' '
+    while continuar not in 'SN':
         continuar = str(input('\033[36mQuer continuar?(S/N)\033[m')).upper().strip()[0]
-    print('--' * 15)
     if continuar == 'N':
+        print('--' * 15)
         break
 print('\033[35m=====--|FIM DO PROGRAMA|--=====\033[m')
 print(f'\033[34mTem \033[m{mais18}\033[34m pessoas com mais de 18 anos.\033[m')
